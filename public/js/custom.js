f966f56f558bada536aa61bdded5354331f743d7
icaynia <icaynia@icaynia.com>
2017-04-25 23:54:48 +0900

    Stable signup

--- public/js/custom.js
+++ public/js/custom.js
@@ -135,7 +135,13 @@ function unfetch(str)
 {
     return str.replaceAll('+', ' ');
 }
+function leadingZeros(n, digits) {
+  var zero = '';
+  n = n.toString();
 
-
-
-
+  if (n.length < digits) {
+    for (var i = 0; i < digits - n.length; i++)
+      zero += '0';
+  }
+  return zero + n;
+}
