65305c9dec86252627ae22be45ddb7ab36cac50a
icaynia <icaynia@icaynia.com>
2017-05-12 11:49:32 +0900

    fix no param

--- router/user/index.js
+++ router/user/index.js
@@ -4,8 +4,11 @@ const authChecker = require('../util/authChecker');
 router.get('/:useruid', function(req, res) {
     var useruid = req.params.useruid;
     var view = '/user/'+useruid;
-    //authChecker.content(req, res, '/user/');
-    res.send(useruid);
+    authChecker.content(req, res, '/user/'+useruid);
+});
+
+router.get('/', function (req, res) {
+    res.send("<script> window.location.href = '/';</script>");
 });
 
 module.exports = router;
