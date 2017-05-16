08e266c9f5fd9ee392f35ee61f5b0be3bfdcf896
icaynia <icaynia@icaynia.com>
2017-05-16 12:23:43 +0900

    fix user page - logged in user's

--- router/view/user/user.controller.js
+++ router/view/user/user.controller.js
@@ -1,17 +1,33 @@
 var request = require('request');
-
+var authChecker = require('../../util/authChecker');
 const Fetch = require('../../util/fetch');
+
 exports.index = (req, res) => {
     var username = req.params.username;
     
     request("http://localhost:3000/api/user/"+username , function (error, response, body) {
         if (!error && response.statusCode == 200) {
             //있는 경우
-            res.render('./pages/user/main', {
-                param: req.params,
-                data: JSON.parse(Fetch.unfetch(body)),
-                search: ""
-            });
+            var mypage = false;
+            authChecker.check(req, function(fsv) {
+                // 프로필이 본인 것일때
+                if (fsv == JSON.parse(body).id) mypage = true;
+                    
+                res.render('./pages/user/main', {
+                        param: req.params,
+                        data: JSON.parse(Fetch.unfetch(body)),
+                        search: "",
+                        mypage: mypage
+                });
+            }, function() {
+                
+                res.render('./pages/user/main', {
+                        param: req.params,
+                        data: JSON.parse(Fetch.unfetch(body)),
+                        search: "",
+                        mypage: mypage
+                });
+            })
         }
         else
         {
@@ -19,3 +35,4 @@ exports.index = (req, res) => {
         }
     });
 }
+
