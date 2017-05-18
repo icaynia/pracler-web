5e1b4ac4a8899d8883f927b2cc925a99e19151ec
icaynia <icaynia@icaynia.com>
2017-05-18 11:54:49 +0900

    Add profile picturea

--- router/user/index.js
+++ router/user/index.js
@@ -1,14 +1,42 @@
 const router = require('express').Router();
 const authChecker = require('../util/authChecker');
+var request = require('request');
 
 router.get('/:useruid', function(req, res) {
     var useruid = req.params.useruid;
-    var view = '/user/'+useruid;
-    authChecker.content(req, res, '/user/'+useruid);
+    var view = '/user/'+useruid+"?mode="+req.param('mode')
+
+    authChecker.content(req, res, view);
 });
 
 router.get('/', function (req, res) {
     res.send("<script> window.location.href = '/';</script>");
 });
+
+router.put('/:username', function(req, res)
+{
+    edit(req,res);
+});
+
+const edit = (req, res) => {
+    var username = req.params.username;
+    
+    request({ 
+            url: "http://localhost:3000/api/user/"+username, 
+            method: 'PUT', 
+            form: req.body
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                //있는 경우
+                res.send({result:1});
+            }
+            else
+            {
+                //없는 경우
+                res.send({result:0});
+            }
+    });
+}
+
 
 module.exports = router;
--- router/view/user/user.controller.js
+++ router/view/user/user.controller.js
@@ -4,7 +4,38 @@ const Fetch = require('../../util/fetch');
 
 exports.index = (req, res) => {
     var username = req.params.username;
-    
+    if (req.param('mode') == 'edit')
+    {
+        request("http://localhost:3000/api/user/"+username , function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            //있는 경우
+            var mypage = false;
+            authChecker.check(req, function(fsv) {
+                // 프로필이 본인 것일때
+                if (fsv == JSON.parse(body).id) mypage = true;
+                    
+                res.render('./pages/user/edit', {
+                        param: req.params,
+                        data: JSON.parse(Fetch.unfetch(body)),
+                        search: "",
+                        mypage: mypage
+                });
+            }, function() {
+                
+                res.render('./pages/user/edit', {
+                        param: req.params,
+                        data: JSON.parse(Fetch.unfetch(body)),
+                        search: "",
+                        mypage: mypage
+                });
+            })
+        }
+        else
+        {
+            res.render('./pages/404');
+        }
+    });
+    } 
     request("http://localhost:3000/api/user/"+username , function (error, response, body) {
         if (!error && response.statusCode == 200) {
             //있는 경우
@@ -35,4 +66,5 @@ exports.index = (req, res) => {
         }
     });
 }
+
 
