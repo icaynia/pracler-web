1cb837a8df3674241d4a2be88fe742b6324d3bc3
icaynia <icaynia@icaynia.com>
2017-05-27 23:49:34 +0900

    Add user history count

--- router/view/history/index.js
+++ router/view/history/index.js
@@ -23,4 +23,23 @@ router.get('/:userid', function(req, res) {
     })
 });
 
+
+router.get('/count/:userid', function(req, res) {
+    var string = encodeURIComponent(Fetch.fetch(req.params.userid));
+    
+    var userid = Fetch.unfetch(req.params.userid);
+
+    request("http://localhost:3000/api/history/count/"+userid , function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            //있는 경우
+            res.json(JSON.parse(body).count);
+        }
+        else
+        {
+            //없는 경우
+            res.render('./pages/404');
+        }
+    })
+});
+
 module.exports = router;
