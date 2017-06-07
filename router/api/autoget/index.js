b282e589debf984a825c66b5f22ca63b88dcdd40
icaynia <icaynia@icaynia.com>
2017-06-08 00:19:23 +0900

    Fix youtubbe icon appear

--- router/api/autoget/index.js
+++ router/api/autoget/index.js
@@ -5,14 +5,20 @@ var parser = new xml2js.Parser();
 
 router.get('/', function (req, res) {
     request({ 
-            url: "http://www.maniadb.com/api/search/metallica/?sr=artist&display=10&key=example&v=0.5", 
+            url: "http://www.maniadb.com/api/search/metallica/?sr=artist&display=100&key=example&v=0.5", 
             method: 'GET', 
             form: req.body
         }, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 //있는 경우
                 parser.parseString(body, function(err, result) {
-                    res.json(result);
+                    var item = result.rss.channel[0].item[0];
+                    for (var i in item['maniadb:majorsongs'][0].song) 
+                    {
+                        var iv = item['maniadb:majorsongs'][0].song[i];
+                        console.log(iv.id[0] + ' ' + item.title[0] + ' - ' + iv.name[0]);
+                    }
+                    res.json(result.rss);
                 });
             }
             else
