0cb1b6752860e7a08abd28d6fbe5cfb2c425bddd
icaynia <icaynia@icaynia.com>
2017-06-28 11:59:22 +0900

    Add grouter what returns count of music, album, artist

--- router/api/history/index.js
+++ router/api/history/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 var request = require('request');
+const Fetch = require('../../util/fetch');
 
 
 router.post('/add', function (req, res) {
@@ -28,6 +29,26 @@ router.get('/:userid', function (req, res) {
         }, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 //있는 경우
+                res.json(JSON.parse(body));
+            }
+            else
+            {
+                //없는 경우
+                res.send({result:0});
+            }
+    });
+});
+
+router.get('/count/music/:artist/:album/:music', function (req, res) {
+    var string = encodeURIComponent(Fetch.fetch(req.params.artist+"/"+req.params.album+"/"+req.params.music));
+    request({ 
+            url: "http://localhost:3000/api/history/count/music/"+req.params.artist+"/"+req.params.album+"/"+req.params.music, 
+            method: 'GET', 
+            form: req.body
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                //있는 경우
+                console.log(body);
                 res.json(JSON.parse(body));
             }
             else
