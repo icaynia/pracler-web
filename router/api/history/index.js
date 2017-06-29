525def69d2f82d44286b345e3dc364cd9ad1a55f
icaynia <icaynia@icaynia.com>
2017-06-29 11:44:42 +0900

    Add artist, album count and set browser title

--- router/api/history/index.js
+++ router/api/history/index.js
@@ -62,6 +62,47 @@ router.get('/count/music/:artist/:album/:music', function (req, res) {
     });
 });
 
+router.get('/count/music/:artist/:album', function (req, res) {
+
+    var artist = encodeURIComponent(Fetch.fetch(req.params.artist));
+    var album = encodeURIComponent(Fetch.fetch(req.params.album));
+    var string = artist + '/' + album;
+    request({ 
+            url: "http://localhost:3000/api/history/count/music/"+string, 
+            method: 'GET', 
+            form: req.body
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                //있는 경우
+                res.send(JSON.parse(body).count+"");
+            }
+            else
+            {
+                //없는 경우
+                res.send({result:0});
+            }
+    });
+});
+router.get('/count/music/:artist', function (req, res) {
+
+    var artist = encodeURIComponent(Fetch.fetch(req.params.artist));
+    var string = artist;
+    request({ 
+            url: "http://localhost:3000/api/history/count/music/"+string, 
+            method: 'GET', 
+            form: req.body
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                //있는 경우
+                res.send(JSON.parse(body).count+"");
+            }
+            else
+            {
+                //없는 경우
+                res.send({result:0});
+            }
+    });
+});
 // 인증 필요
 router.put('/delete/:oid', function(req, res) {
     
